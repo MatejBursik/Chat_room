b9ad125cdba3071f2f04da24f4e58909049af5bd
MatejBursik <r0929146@student.thomasmore.be>
2024-09-23 02:25:35 +0200

    added Friend table to database, updated diagrams to explain Friend table, renamed folder diagrams to documentation

--- database/data_init.sql
+++ database/data_init.sql
@@ -4,6 +4,8 @@ USE chat_room_db;
 INSERT INTO users (username, password, pictureURL, color) VALUES ('john_doe', 'password1', 'john_doe.png', '#2144fb');
 INSERT INTO users (username, password, pictureURL, color) VALUES ('jane_doe', 'password2', 'jane_doe.png', '#631ac6');
 INSERT INTO chat_rooms (name, pictureURL, owner) VALUES ('test_room', 'test_room.png', 1);
+INSERT INTO can_access (userID, chatRoomID) VALUES (1, 1);
 INSERT INTO can_access (userID, chatRoomID) VALUES (2, 1);
 INSERT INTO messages (messageText, sendBy, chatRoomID) VALUES ('test 1', 1, 1);
 INSERT INTO messages (messageText, sendBy, chatRoomID) VALUES ('test 2', 2, 1);
+INSERT INTO friends (userID1, userID2) VALUES (1, 2);
