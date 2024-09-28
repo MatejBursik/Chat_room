c72a964e820ce278cc82e68094d880ca2d853d6b
MatejBursik <r0929146@student.thomasmore.be>
2024-09-28 04:23:18 +0200

    created view_account.html, added paths to the web main controller

--- app/chat_room_web/src/main/java/com/web/chat_room/controller/MainController.java
+++ app/chat_room_web/src/main/java/com/web/chat_room/controller/MainController.java
@@ -6,9 +6,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController{
 
-    @RequestMapping("/index")
-        public String index() {
-            return "index";
-        }
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 
+    @RequestMapping("/chatroom")
+    public String chatroom() {
+        return "chatroom";
+    }
+
+    @RequestMapping("/create_chatroom")
+    public String create_chatroom() {
+        return "create_chatroom";
+    }
+
+    @RequestMapping("/manage_chatroom")
+    public String manage_chatroom() {
+        return "manage_chatroom";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping("/view_acoount")
+    public String view_acoount() {
+        return "view_acoount";
+    }
+
+    @RequestMapping("/manage_acoount")
+    public String manage_acoount() {
+        return "manage_acoount";
+    }
 }
